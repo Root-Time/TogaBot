1957144bdfbb91a14d847b223b82a6f5daa36424
Root-Time <76535404+Root-Time@users.noreply.github.com>
2021-10-25 19:38:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,5 +19,4 @@ bot.load_extension("cogs.LevelSystem")
 #bot.load_extension("cogs.Welcome")
 
     
-#bot.run('NzM2NjMxNDgwMjc2NjE1MTc0.Xxxnhg.MaPa-QCK0hmS4nIJtqjfZpvDMAg')
-bot.run("ODkxNzcwNTg2MjMyMTMxNjA0.YVDMFA.J7Xg8Nu0XjA2AZX3ScWgTt4PlYg")
+bot.run(TOKEN)
